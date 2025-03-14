731a1515bf7c97a47a56a7da0883804e1cb46362
Erick Hernandez <erickhernandez0492@gmail.com>
2025-03-14 23:26:17 +0100

    feat: ActivatedRoute y queryParams

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input, output, signal } from '@angular/core';
+import { Component, effect, input, linkedSignal, output } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -7,10 +7,11 @@ import { Component, effect, input, output, signal } from '@angular/core';
 })
 export class SearchInputComponent {
   placeholder = input<string>('Buscar');
+  debounceTime = input(1000);
+  initialValue = input<string>('');
   value = output<string>();
-  debounceTime = input(300);
 
-  inputValue = signal<string>('');
+  inputValue = linkedSignal<string>(() => this.initialValue() ?? '');
 
   debounceEffect = effect((onCleanup) => {
     const value = this.inputValue();
--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,9 +1,25 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 import { of } from 'rxjs';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { Region } from '../../interfaces/region.type';
 import { CountryService } from '../../services/country.service';
+
+function validateQueryParam(queryParam: string): Region {
+  queryParam = queryParam.toLowerCase();
+
+  const validRegions: Record<string, Region> = {
+    africa: 'Africa',
+    americas: 'Americas',
+    asia: 'Asia',
+    europe: 'Europe',
+    oceania: 'Oceania',
+    antarctic: 'Antarctic',
+  };
+
+  return validRegions[queryParam] ?? 'Americas';
+}
 
 @Component({
   selector: 'app-by-region-page',
@@ -21,12 +37,25 @@ export class ByRegionPageComponent {
     'Antarctic',
   ];
 
-  selectedRegion = signal<Region | null>(null);
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('region') ?? '';
+
+  selectedRegion = linkedSignal<Region>(() =>
+    validateQueryParam(this.queryParam)
+  );
 
   countryResource = rxResource({
     request: () => ({ region: this.selectedRegion() }),
     loader: ({ request }) => {
       if (!request.region) return of([]);
+
+      this.router.navigate(['/country/by-region'], {
+        queryParams: {
+          region: request.region,
+        },
+      });
 
       return this.countryService.searchByRegion(request.region);
     },
